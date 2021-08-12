5474feab2bb4be072a0b6da914225dcfca5fe090
zyx <zyx@softline.sh.cn>
2021-08-12 16:14:18 +0800

    Initial commit

---
+++ src/test/java/org/xixixi/RedisTestTest.java
@@ -0,0 +1,20 @@
+package org.xixixi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RedisTestTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
